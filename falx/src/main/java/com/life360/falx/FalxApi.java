f50e2523856bdb3b94d89606bc372501d26d9640
Remon Karim <remon@life360.com>
2017-07-07 14:39:47 -0700

    Project Falx library - first commit, only library project and a test app.

---
+++ falx/src/main/java/com/life360/falx/FalxApi.java
@@ -0,0 +1,17 @@
+package com.life360.falx;
+
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Created by remon on 6/27/17.
+ */
+public class FalxApi {
+
+    private static final String TAG = "FalxApi";
+
+    public static void init(final Context context) {
+        Log.d(TAG, "init Falx");
+
+    }
+}
