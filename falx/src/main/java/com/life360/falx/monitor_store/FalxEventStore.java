853809a642fd09437c904478af8a817d1f0c90b7
Vikas Sable <vikassable2009@gmail.com>
2018-02-27 11:50:59 -0800

    LIFE-15880 added realm operation on single thread

--- falx/src/main/java/com/life360/falx/monitor_store/FalxEventStore.java
+++ falx/src/main/java/com/life360/falx/monitor_store/FalxEventStore.java
@@ -2,6 +2,7 @@ package com.life360.falx.monitor_store;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
@@ -24,10 +25,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.functions.Consumer;
-import io.reactivex.schedulers.Schedulers;
+import io.reactivex.functions.Function;
 import io.realm.OrderedRealmCollectionSnapshot;
 import io.realm.Realm;
 import io.realm.RealmList;
@@ -43,26 +47,26 @@ public class FalxEventStore implements FalxEventStorable {
     private static final String TAG = FalxEventStore.class.getSimpleName();
 
     private static final String FALX_EVENT_STORE_SYNC_DATE_KEY = "FalxEventStoreSyncDatekey";
-    private static final String FALX_LOG_SYNC_PREF_FILE = "falx_sync_log_pref_file";
-
+    private static final String LOOPER_THREAD_NAME = "LOOPER_SCHEDULER";
     //TODO move to network monitor
     public static final String FALX_URL_PREFIX = "URL=";
-    /**
-     * sync value is in hrs
-     */
-    private static final int SYNC_INTERVAL = 24;
 
     protected Context context;
-
     private RealmStore realmStore;
-
     private CompositeDisposable compositeDisposable;
+    private final HandlerThread handlerThread;
+    private final Scheduler looperScheduler;
 
     public FalxEventStore(RealmStore store, Context context) {
         this.realmStore = store;
         this.context = context;
         compositeDisposable = new CompositeDisposable();
 
+        handlerThread = new HandlerThread("LOOPER_SCHEDULER");
+        handlerThread.start();
+        synchronized (handlerThread) {
+            looperScheduler = AndroidSchedulers.from(handlerThread.getLooper());
+        }
         Realm.init(context);
     }
 
@@ -78,11 +82,28 @@ public class FalxEventStore implements FalxEventStorable {
 
     @Override
     public void save(final FalxMonitorEvent event) {
+        if (Thread.currentThread().getName().equals(LOOPER_THREAD_NAME)) {
+            System.out.println("already in a thread : " + Thread.currentThread().getName());
+            saveEvent(event);
+        } else {
+            Flowable.just(1)
+                    .subscribeOn(looperScheduler)
+                    .observeOn(looperScheduler)
+                    .doOnNext(new Consumer<Integer>() {
+                        @Override
+                        public void accept(Integer integer) throws Exception {
+                            System.out.println("executing on : " + Thread.currentThread().getName());
+                            saveEvent(event);
+                        }
+                    })
+                    .blockingFirst();
+        }
+    }
 
+    private void saveEvent(final FalxMonitorEvent event) {
         Realm realm = null;
         try {
             realm = realmStore.realmInstance();
-
             realm.executeTransaction(new Realm.Transaction() {
                 @Override
                 public void execute(@NonNull Realm realm) {
@@ -117,8 +138,27 @@ public class FalxEventStore implements FalxEventStorable {
 
     @Override
     public void deleteOldEvents() {
+        if (Thread.currentThread().getName().equals(LOOPER_THREAD_NAME)) {
+            System.out.println("already in a thread : " + Thread.currentThread().getName());
+            deleteOldEventsFromDataStore();
+        } else {
+            Flowable.just(1)
+                    .subscribeOn(looperScheduler)
+                    .observeOn(looperScheduler)
+                    .doOnNext(new Consumer<Integer>() {
+                        @Override
+                        public void accept(Integer integer) throws Exception {
+                            System.out.println("executing on : " + Thread.currentThread().getName());
+                            deleteOldEventsFromDataStore();
+                        }
+                    })
+                    .blockingFirst();
+        }
+    }
+
+    private void deleteOldEventsFromDataStore() {
         Calendar sixDaysAgoCal = Calendar.getInstance();
-        sixDaysAgoCal.add(Calendar.DATE, -6);
+        sixDaysAgoCal.add(Calendar.DATE, 1);
         sixDaysAgoCal.set(Calendar.HOUR_OF_DAY, 0);
         sixDaysAgoCal.set(Calendar.MINUTE, 0);
         sixDaysAgoCal.set(Calendar.SECOND, 0);
@@ -155,6 +195,23 @@ public class FalxEventStore implements FalxEventStorable {
 
     @Override
     public void deleteAllEvents() {
+        if (Thread.currentThread().getName().equals(LOOPER_THREAD_NAME)) {
+            deleteAllEventsFromDataStore();
+        } else {
+            Flowable.just(1)
+                    .subscribeOn(looperScheduler)
+                    .observeOn(looperScheduler)
+                    .doOnNext(new Consumer<Integer>() {
+                        @Override
+                        public void accept(Integer integer) throws Exception {
+                            deleteAllEventsFromDataStore();
+                        }
+                    })
+                    .blockingFirst();
+        }
+    }
+
+    private void deleteAllEventsFromDataStore() {
         Realm realm = null;
         try {
             realm = realmStore.realmInstance();
@@ -184,22 +241,36 @@ public class FalxEventStore implements FalxEventStorable {
      * @return a list of events, can be empty if no stored events found
      */
     @Override
-    public List<AggregatedFalxMonitorEvent> aggregatedEvents(String eventName, boolean allowPartialDays) {
+    public List<AggregatedFalxMonitorEvent> aggregatedEvents(final String eventName, final boolean allowPartialDays) {
+        if (Thread.currentThread().getName().equals(LOOPER_THREAD_NAME)) {
+            return aggregatedEventsFromDataStore(eventName, allowPartialDays);
+        } else {
+            return Flowable.just(1)
+                    .subscribeOn(looperScheduler)
+                    .observeOn(looperScheduler)
+                    .map(new Function<Integer, List<AggregatedFalxMonitorEvent>>() {
+                        @Override
+                        public List<AggregatedFalxMonitorEvent> apply(Integer integer) throws Exception {
+                            return aggregatedEventsFromDataStore(eventName, allowPartialDays);
+                        }
+                    })
+                    .blockingFirst();
+        }
+    }
+
+    private List<AggregatedFalxMonitorEvent> aggregatedEventsFromDataStore(String eventName, boolean allowPartialDays) {
         Realm realm = null;
         try {
             List<AggregatedFalxMonitorEvent> aggregatedFalxEvents = new ArrayList<>();
             realm = realmStore.realmInstance();
-
             RealmResults<FalxEventEntity> events = realm.where(FalxEventEntity.class)
                     .equalTo(FalxEventEntity.KEY_NAME, eventName)
                     .equalTo(FalxEventEntity.KEY_PROCESSED_FOR_AGGREGATION, false)
                     .findAllSorted(FalxEventEntity.KEY_TIMESTAMP, Sort.ASCENDING);
             int count = events.size();
-
             if (count <= 0) {
                 return aggregatedFalxEvents;
             }
-
             RealmList<FalxEventEntity> processedEvents = new RealmList<>();
             Date beginWindowDate = events.first().getTimestamp();
             Map<String, Double> aggregatedArguments = new HashMap<>();
@@ -210,7 +281,6 @@ public class FalxEventStore implements FalxEventStorable {
                 calBegin.setTime(beginWindowDate);
                 Calendar calCurrent = Calendar.getInstance();
                 calCurrent.setTime(currentEventDate);
-
                 if (isSameDate(calBegin, calCurrent)) {
                     this.aggregateCurrentEvent(
                             event,
@@ -222,7 +292,6 @@ public class FalxEventStore implements FalxEventStorable {
                             processedEvents,
                             aggregatedArguments,
                             aggregatedFalxEvents);
-
                     //for next event
                     processedEvents.clear();
                     aggregatedArguments.clear();
@@ -233,7 +302,6 @@ public class FalxEventStore implements FalxEventStorable {
                             aggregatedArguments);
                 }
             }
-
             if (allowPartialDays) {
                 appendEvent(
                         beginWindowDate,
@@ -249,13 +317,12 @@ public class FalxEventStore implements FalxEventStorable {
         }
     }
 
+
     private void aggregateCurrentEvent(FalxEventEntity currentEntity,
                                        List<FalxEventEntity> processedEvents,
                                        Map<String, Double> aggregatedArguments) {
         processedEvents.add(currentEntity);
-
         Map<String, Double> currentArguments = currentEntity.getArgumentsMap();
-
         for (String key : currentArguments.keySet()) {
             if (!key.contains(FALX_URL_PREFIX)) {
                 if (aggregatedArguments.containsKey(key)) {
@@ -265,7 +332,6 @@ public class FalxEventStore implements FalxEventStorable {
                 }
             }
         }
-
     }
 
     private AggregatedFalxMonitorEvent createAggregatedEvents(final RealmList<FalxEventEntity> events,
@@ -276,16 +342,13 @@ public class FalxEventStore implements FalxEventStorable {
             if (events == null || events.size() == 0) {
                 return null;
             }
-
             AggregatedFalxMonitorEvent aggregatedEvent = new AggregatedFalxMonitorEvent(
                     events.first().getName(),
                     events.size(),
                     timestamp);
-
             for (Map.Entry<String, Double> entry : aggregatedArguments.entrySet()) {
                 aggregatedEvent.putArgument(entry.getKey(), entry.getValue());
             }
-
             realm = realmStore.realmInstance();
             realm.executeTransaction(new Realm.Transaction() {
                 @Override
@@ -313,31 +376,45 @@ public class FalxEventStore implements FalxEventStorable {
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         Date startOfDay = cal.getTime();
-
         AggregatedFalxMonitorEvent aggregatedFalxMonitorEvent =
                 createAggregatedEvents(processedEvents, aggregatedArguments, startOfDay.getTime());
-
         if (aggregatedFalxMonitorEvent != null) {
             aggregatedFalxEvents.add(aggregatedFalxMonitorEvent);
         }
     }
 
     @Override
-    public List<AggregatedFalxMonitorEvent> allAggregatedEvents(boolean allowPartialDays) {
+    public List<AggregatedFalxMonitorEvent> allAggregatedEvents(final boolean allowPartialDays) {
+        if (Thread.currentThread().getName().equals(LOOPER_THREAD_NAME)) {
+            return allAggregatedEventsFromDatastore(allowPartialDays);
+        } else {
+            return Flowable.just(1)
+                    .subscribeOn(looperScheduler)
+                    .observeOn(looperScheduler)
+                    .map(new Function<Integer, List<AggregatedFalxMonitorEvent>>() {
+                        @Override
+                        public List<AggregatedFalxMonitorEvent> apply(Integer integer) throws Exception {
+                            return allAggregatedEventsFromDatastore(allowPartialDays);
+                        }
+                    })
+                    .blockingFirst();
+        }
+    }
+
+    private List<AggregatedFalxMonitorEvent> allAggregatedEventsFromDatastore(boolean allowPartialDays) {
         Realm realm = null;
         try {
             realm = realmStore.realmInstance();
             RealmResults<FalxEventEntity> allDistinctNameEvents = realm.where(FalxEventEntity.class).distinct(FalxEventEntity.KEY_NAME);
             List<AggregatedFalxMonitorEvent> allEvents = new ArrayList<>();
-
             for (int i = 0; i < allDistinctNameEvents.size(); i++) {
-                List<AggregatedFalxMonitorEvent> events = this.aggregatedEvents(
+                List<AggregatedFalxMonitorEvent> events = aggregatedEvents(
                         allDistinctNameEvents.get(i).getName(),
                         allowPartialDays);
                 allEvents.addAll(events);
             }
-            this.setSyncDate(new Date());
-            this.deleteOldEvents();
+            setSyncDate(new Date());
+            deleteOldEvents();
             return allEvents;
         } finally {
             if (realm != null) {
@@ -347,54 +424,68 @@ public class FalxEventStore implements FalxEventStorable {
     }
 
     @Override
-    public URI eventToJSONFile(@NonNull String fileName) {
+    public URI eventToJSONFile(@NonNull final String fileName) {
+        //using dummy URI so we are not passing null in reactive stream
+        final URI dummyUri = URI.create("file://dummy");
+        URI uri;
+        if (Thread.currentThread().getName().equals(LOOPER_THREAD_NAME)) {
+            uri = getURIForJSONFile(fileName, dummyUri);
+        } else {
+            uri = Flowable.just(1)
+                    .subscribeOn(looperScheduler)
+                    .observeOn(looperScheduler)
+                    .map(new Function<Integer, URI>() {
+                        @Override
+                        public URI apply(Integer integer) throws Exception {
+                            return getURIForJSONFile(fileName, dummyUri);
+                        }
+                    })
+                    .blockingFirst();
+        }
+
+        return uri == dummyUri ? null : uri;
+    }
+
+    private URI getURIForJSONFile(String fileName, URI dummyUri) {
+        System.out.println("testing thread name :" + Thread.currentThread().getName());
         Realm realm = null;
         try {
-            this.deleteOldEvents();
-
+            deleteOldEvents();
             if (fileName.length() == 0) {
-                return null;
+                return dummyUri;
             }
-
             File jsonFile = new File(context.getCacheDir(), fileName);
             realm = realmStore.realmInstance();
-
             RealmResults<FalxEventEntity> allRealmEvents = realm.where(FalxEventEntity.class)
                     .findAllSorted(FalxEventEntity.KEY_TIMESTAMP, Sort.ASCENDING);
-
             if (allRealmEvents.size() <= 0) {
-                return null;
+                return dummyUri;
             }
-
             JSONArray jsonEvents = new JSONArray();
             for (FalxEventEntity falxEventEntity : allRealmEvents) {
                 JSONObject jsonObject = new JSONObject();
                 try {
                     jsonObject.put(FalxEventEntity.KEY_NAME, falxEventEntity.getName());
-
                     Calendar cal = Calendar.getInstance();
                     cal.setTime(falxEventEntity.getTimestamp());
                     jsonObject.put(FalxEventEntity.KEY_TIMESTAMP, Long.toString(cal.getTimeInMillis()));
-
                     for (Map.Entry<String, Double> entry : falxEventEntity.getArgumentsMap().entrySet()) {
                         jsonObject.put(entry.getKey(), Double.toString(entry.getValue()));
                     }
                 } catch (JSONException e) {
                     Log.d(TAG, e.getMessage());
-                    return null;
+                    return dummyUri;
                 }
                 jsonEvents.put(jsonObject);
             }
-
             FileOutputStream fileOutputStream = null;
             try {
                 fileOutputStream = new FileOutputStream(jsonFile, false);
                 byte[] content = jsonEvents.toString().getBytes();
-
                 fileOutputStream.write(content);
             } catch (IOException e) {
                 Log.e(TAG, e.getMessage());
-                return null;
+                return dummyUri;
             } finally {
                 if (fileOutputStream != null)
                     try {
@@ -410,14 +501,12 @@ public class FalxEventStore implements FalxEventStorable {
                 realm.close();
             }
         }
-
-
     }
 
     @Override
     public void subscribeToEvents(Observable<FalxMonitorEvent> observable) {
         compositeDisposable.add(observable
-                .subscribeOn(Schedulers.io())
+                .observeOn(looperScheduler)
                 .subscribe(new Consumer<FalxMonitorEvent>() {
                     @Override
                     public void accept(FalxMonitorEvent event) throws Exception {
@@ -435,7 +524,6 @@ public class FalxEventStore implements FalxEventStorable {
         return (cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
                 cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
                 cal1.get(Calendar.DAY_OF_MONTH) == cal2.get(Calendar.DAY_OF_MONTH));
-
     }
 
     // Test function
@@ -443,7 +531,6 @@ public class FalxEventStore implements FalxEventStorable {
         Realm realm = null;
         try {
             realm = realmStore.realmInstance();
-
             RealmResults<FalxEventEntity> entities = realm.where(FalxEventEntity.class).findAll();
             for (FalxEventEntity entity : entities) {
                 Log.d(TAG, entity.toString() + "----" + entity.getArgumentsMap().toString());
